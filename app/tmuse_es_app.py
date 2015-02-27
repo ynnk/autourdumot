cb899c267063adb02d16dc392360c52d2b853d62
ynnk <ychudy@gmail.com>
2015-02-27 19:20:06 +0100

    relative path for caching wk

--- app/tmuse_es_app.py
+++ app/tmuse_es_app.py
@@ -19,7 +19,7 @@ app.debug = True
 logger = get_basic_logger(logging.DEBUG)
 
 import requests_cache
-requests_cache.install_cache('./cache/demo_cache')
+requests_cache.install_cache('../cache/demo_cache.sqlite')
 
 # remote api
 #tmuseApi = RemoteApi("http://carton.kodexlab.com/tmuse_alpha/tmuse_v1")
