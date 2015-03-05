5f1603e21a526a1eb254efd3002ffd36db2e0582
ynnk <ychudy@gmail.com>
2015-03-05 19:16:28 +0100

    -> pas de police par default

--- app/static/materials.js
+++ app/static/materials.js
@@ -65,7 +65,7 @@ define(['cello_gviz'], function(gviz) {
                         
                         'fontScale'  :  0.2,
                         'line_max_length': 12,
-                        'font' : "normal 10px Helvetica",
+                        'font' : "normal 10px",
                         'fontFillStyle'  : '#333',  //#366633',           
                         //'fontStrokeStyle'  : '#F06',
                         //'fontStrokeWidth' : .1,
