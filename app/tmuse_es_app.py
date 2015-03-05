09388e78408a187327c5bfeb84689a01e2376313
ynnk <ychudy@gmail.com>
2015-03-05 01:13:17 +0100

    debug in env export APP_DEBUG=true

--- app/tmuse_es_app.py
+++ app/tmuse_es_app.py
@@ -16,7 +16,7 @@ import wiktionary as wk
 
 # Build the app & 
 app = Flask(__name__)
-app.debug = os.environ['DEBUG'] == "true"
+app.debug = os.environ['APP_DEBUG'] == "true"
 logger = get_basic_logger(logging.DEBUG)
 
 # ajout la config pour le tracker Piwik
