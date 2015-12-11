73ca9c77b56d1dbfea5e81d5463d81b875e80c2b
Emmanuel N <enavarro222@gmail.com>
2015-12-11 22:38:19 +0100

    Cosmetic on glaff parser

--- glaff.py
+++ glaff.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
-import sys
-import argparse
-
 """
 GLÀFF is based on Wiktionnaire, and constitutes a derivative work from this resource.
 It is available under the same licence: 
@@ -20,7 +17,30 @@ homepage : http://redac.univ-tlse2.fr/lexiques/glaff.html
 dl : http://redac.univ-tlse2.fr/lexiques/glaff/GLAFF-1.2.1.tar.bz2  
 """
 
+import sys
+import argparse
+
+
 LICENCE = "WTFL" or "Creative Commons By-SA 3.0"
+
+
+def parse(path):
+    glaff = {}
+    try:
+        with open(path, 'r') as fin:
+            for line in fin:
+                spl = line.split('|')
+                form , pos, lemma = spl[:3]
+                pos = pos[0]
+
+                key = '%s.%s' % (pos, lemma)
+                entry = glaff.get(key,[])
+                entry.append(form)
+                glaff[key] = entry
+    except:
+        print("Error while parsing Glaff data \nAre you sure glaff is there ?")
+    return glaff
+
 
 def main():
     """ Function doc
@@ -32,26 +52,7 @@ def main():
 
     path = "GLAFF-1.2.1/glaff-1.2.1.txt"
     glaff = parse(path)
-    print len(glaff)
-
-    
-def parse(path):
-    glaff = {}
-    try : 
-        with open(path, 'r') as fin:
-            for line in fin:
-                spl = line.split('|')
-                form , pos, lemma = spl[:3]
-                pos = pos[0]
-                
-                key = '%s.%s' % (pos, lemma)
-                entry = glaff.get(key,[])
-                entry.append(form)
-                glaff[key] = entry
-    except :
-        print "Error while parsing Glaff data \nAre you sure glaff is there ?"    
-        
-    return glaff
+    print(len(glaff))
 
 if __name__ == '__main__':
     sys.exit(main())
