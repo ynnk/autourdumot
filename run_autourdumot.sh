1a1ea543106f4820437b2751be90ff4f42c4b05d
Emmanuel N <enavarro222@gmail.com>
2015-12-11 23:18:55 +0100

    Rename & update main app and run script

--- run_autourdumot.sh
+++ run_autourdumot.sh
@@ -3,22 +3,22 @@
 
 #set -x  #log all execed lin for debug
 set -e
-BASEDIR=/var/wwwapps/tmuse_alpha_v2/tmuse
-APPMODULE=tmuse_es_app
+BASEDIR=/var/wwwapps/autourdumot
+APPMODULE=autourdumot
 APPNAME=app
 
 # log
-LOGFILE=$BASEDIR/log/tmuse.log
+LOGFILE=$BASEDIR/log/autourdumot.log
 LOGDIR=$(dirname $LOGFILE)
-LOGLEVEL=debug
+LOGLEVEL="info"
 
 # gunicorn config
-BIND=0.0.0.0:8051
+BIND=0.0.0.0:8125
 NUM_WORKERS=1
 
 # user/group to run as
-USER=wapps
-GROUP=wapps
+USER=wwwapps
+GROUP=wwwapps
 
 ## start the app
 cd $BASEDIR
@@ -34,8 +34,9 @@ test -d $LOGDIR || mkdir -p $LOGDIR
 cd app
 
 # set Elastic Search host
-export ES_HOST=192.168.122.90:9200
-
+export ES_HOST="localhost:9200"
+export ES_INDEX="autourdumot"
+export APP_DEBUG=false
 
 # run the gunicorn server
 exec gunicorn --workers $NUM_WORKERS --bind=$BIND\
